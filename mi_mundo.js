ef98084ea1fd9770b8ec1ffb1f0bc33fdc7a58a6
Matiasrosello2009 <136343878+Matiasrosello2009@users.noreply.github.com>
2025-04-07 21:56:45 -0300

    REPASO 4.0

---
+++ mi_mundo.js
@@ -0,0 +1,6 @@
+let parrafo = document.getElementById('miParrafo');
+let boton = document.getElementById('miBoton');
+
+function cambiarColorParrafo() {
+    if boton = onclick (document.getElementById('parrafoCambiante').style.backgroundColor = 'lightblue';")
+}
