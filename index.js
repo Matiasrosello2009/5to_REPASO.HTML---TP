262376c796a49148cab8f3628c49fc9fda546dba
alumno <alumno@Instituto.local>
2025-05-14 08:18:41 -0300

    practica 6.0

---
+++ index.js
@@ -0,0 +1,11 @@
+let colores = ['rojo', 'azul', 'verde', 'amarillo', 'naranja']; 
+
+let primero = colores[0]
+let ultimo = [colores.length -3]
+
+console.log(colores.length);
+
+colores.push('rosa'); 
+['rojo', 'azul', 'verde', 'amarillo', 'naranja', 'rosa']; 
+
+colores.pop()
