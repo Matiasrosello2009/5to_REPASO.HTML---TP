b03946da1befb131bfdee736a245578fb8712c49
alumno <alumno@Instituto.local>
2025-05-15 11:12:47 -0300

    practica 7.0

---
+++ arreglos.js
@@ -0,0 +1,22 @@
+let colores = ['rojo', 'azul', 'verde', 'amarillo', 'naranja']; 
+
+console.log(colores[0])
+
+console.log(colores.length[-1]);
+
+console.log('El tamaño del arreglo es ' + colores.length[5])
+
+colores.push('Violeta')
+console.log(colores[colores.length -1])
+
+colores.unshift('bordo')
+console.log(colores[0])
+
+colores.pop()
+console.log(colores)
+
+colores.shift()
+console.log(colores)
+
+colores[1] = 'marron' 
+console.log(colores)
