0e7c11d328af97e3e92762bb398812cccf9bbb7c
alumno <alumno@Instituto.local>
2025-07-31 11:15:51 -0300

    practica 13

--- objetos_java.js
+++ objetos_java.js
@@ -42,3 +42,46 @@ let estudiantes = [
 for (let i=0; i < estudiantes.length; i++){
     console.log(estudiantes[i].apellido + ' ' + estudiantes[i].nombre)
 }
+
+let estudiantesIVT = [
+    {
+        nombre: 'Agustín',
+        apellido: 'Ávila',
+        edad: 16,
+        notaExamen: 1
+    }, 
+    {
+        nombre: 'Jeronimo',
+        apellido: 'Orfila Bohnet',
+        edad: 16,
+        notaExamen: 9
+    }, 
+    {
+        nombre: 'Dámaris',
+        apellido: 'Villegas',
+        edad: 16,
+        notaExamen: 5
+    }, 
+    {
+        nombre: 'Jazmin',
+        apellido: 'Rodriguez',
+        edad: 16,
+        notaExamen: 4
+    }, 
+    {
+        nombre: 'Antonella',
+        apellido: 'Lattandi',
+        edad: 16,
+        notaExamen: 5.9
+    }, 
+    {
+        nombre: 'Matias',
+        apellido: 'Rosello',
+        edad: 16,
+        notaExamen: 10
+    }, 
+]
+
+for (let i=0; i < estudiantesIVT.length; i++){
+    console.log(estudiantesIVT[i].apellido + ' ' + estudiantesIVT[i].nombre + 'Se sacó un ' + estudiantesIVT[i].notaExamen)
+}
