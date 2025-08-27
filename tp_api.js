a15e571b244b0778cb7bb1d8b119a217d2431691
alumno <alumno@Instituto.local>
2025-08-27 08:14:50 -0300

    practica 23.1

--- tp_api.js
+++ tp_api.js
@@ -12,14 +12,3 @@ boton.onClick = function (){
             imagen.src = datos.sprites.front_shiny
         })
 }
-
-fetch('https://pokeapi.co/api/v2/pokemon-form/157/')
-
-.then(respuesta => respuesta.json())
-.then(datos => {
-    console.log(datos.results)
-    for (let i = 0; i < datos.results.length; i++){
-        parrafo.innerHTML = parrafo.textContent + ' ' + datos.results[i].name
-        console.log(datos.results[i].name)
-    }
-})
